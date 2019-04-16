bfae6e2eafd6b50097bd5e33d4a237700cd74524
ForceUser <forceuserz@gmail.com>
2019-04-16 18:20:31 +0300

    0.0.5 - fix readline

--- index.js
+++ index.js
@@ -12,32 +12,38 @@ const __dirname = path.dirname(new URL(import.meta.url).pathname);
 const cwd = process.cwd();
 const argv = yargs(process.argv.slice(3)).argv;
 
+async function readln (question) {
+	let result;
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+		terminal: true,
+	});
+	try {
+		result = await rl.questionAsync(question);
+	}
+	finally {
+		rl.close();
+	}
+	return result;
+}
+
 
 
 async function main () {
 	const data = {};
 
 	try {
-		const rl = readline.createInterface({
-			input: process.stdin,
-			output: process.stdout,
-			terminal: true,
-		});
-		try {
-			data.name = await rl.questionAsync("Enter project name: ");
-			data.owner = await rl.questionAsync("Enter github project owner: ");
-			data.src = path.resolve(__dirname, "./project");
-			data.dest = path.resolve(cwd, `./${data.name}`);
-			await fs.copy(data.src, data.dest, {overwrite: true});
-			const pkg = JSON.parse((await fs.readFile(path.resolve(data.dest, "./package.json"), "utf8")).replace(/projectname/gm, data.name));
-			pkg["va-release"].owner = data.owner;
-			pkg["va-release"].library = camelcase(data.name);
-			await fs.writeFile(path.resolve(data.dest, "./package.json"), JSON.stringify(pkg, null, "\t"), "utf8");
-			console.log(`${"Success!".green} Project ${`${data.name}`.cyan} created at ${`${data.dest}`.cyan}`);
-		}
-		finally {
-			rl.close();
-		}
+		data.name = await readln("Enter project name: ");
+		data.owner = await readln("Enter github project owner: ");
+		data.src = path.resolve(__dirname, "./project");
+		data.dest = path.resolve(cwd, `./${data.name}`);
+		await fs.copy(data.src, data.dest, {overwrite: true});
+		const pkg = JSON.parse((await fs.readFile(path.resolve(data.dest, "./package.json"), "utf8")).replace(/projectname/gm, data.name));
+		pkg["va-release"].owner = data.owner;
+		pkg["va-release"].library = camelcase(data.name);
+		await fs.writeFile(path.resolve(data.dest, "./package.json"), JSON.stringify(pkg, null, "\t"), "utf8");
+		console.log(`${"Success!".green} Project ${`${data.name}`.cyan} created at ${`${data.dest}`.cyan}`);
 	}
 	catch (error) {
 		console.log("ERROR", error);
