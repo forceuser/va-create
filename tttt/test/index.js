10e4d80a42408a9c2005a4e68da517b84d756989
ForceUser <forceuserz@gmail.com>
2019-04-17 04:41:48 +0300

    0.0.9 - improved interactive menu

--- tttt/test/index.js
+++
@@ -1,7 +0,0 @@
-import test from "tape";
-import sinon from "sinon";
-
-test("empty test", t => {
-	t.ok(true, "test passed :)");
-	t.end();
-});
--- tttt/tonic-example.js
+++
@@ -1,1 +0,0 @@
-const projectnameCamel = require("projectname");
