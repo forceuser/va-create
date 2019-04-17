8d5b339a14657b139896882763415364e5667ae7
ForceUser <forceuserz@gmail.com>
2019-04-17 04:40:49 +0300

    0.0.8 - improved interactive menu

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import process from "process";
 import readline from "readline-promise";
 import shell from "shelljs";
-import path from "path";
+import $path from "path";
 import fs from "fs-extra";
 import yargs from "yargs";
+import inquirer from "inquirer";
+import merge from "deepmerge";
 import camelcase from "camelcase";
 import {URL} from "universal-url";
 import "colors";
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const __dirname = $path.dirname(new URL(import.meta.url).pathname);
 const cwd = process.cwd();
 const argv = yargs(process.argv.slice(3)).argv;
 
@@ -27,30 +29,144 @@ async function readln (question) {
 	}
 	return result;
 }
+const fileRules = {
+	"-npmignore": {rename: ".npmignore"},
+	"package": {include: false},
+	"babel.config.js": {include: ["client", "lib", "babel"]},
+	".browserslistrc": {include: ["client", "lib", "babel"]},
+	"webpack": {include: ["client", "lib"]},
+	".travis.yml": {include: ["travis"]},
+	"dist": {include: ["client", "lib"]},
+	"src/server.js": {include: ["server"]},
+	"src/cli.js": {include: ["cli"]},
+	"src/index.js": {include: ["client", "lib"]},
+	"utils/dev-server.js": {include: ["client"]},
+	"utils/invoke-middleware.js": {include: ["client"]},
+};
+async function copy (src, dest, flags) {
+	function isIncluded (include) {
+		return include == null ||
+		include === true ||
+		(
+			Array.isArray(include) &&
+			include.some(i => flags.includes(i))
+		);
+	}
+	const root = src;
+	async function copyDir (src, dest) {
+		const entries = await fs.readdir(src, {withFileTypes: true});
+		await fs.mkdir(dest);
+		for (const entry of entries) {
+			const srcPath = $path.resolve(src, entry.name);
+			const relPath = $path.relative(root, srcPath);
+			const fileRule = fileRules[relPath];
+			// console.log("relPath", relPath);
+			if (!fileRule || (fileRule && isIncluded(fileRule.include))) {
+				const destPath = $path.resolve(dest, fileRule && fileRule.rename ? fileRule.rename : entry.name);
+				if (entry.isDirectory()) {
+					await copyDir(srcPath, destPath);
+				}
+				else {
+					await fs.copyFile(srcPath, destPath);
+				}
+			}
+
+		}
+	}
+
+	return copyDir(src, dest);
+}
 
 
 
 async function main () {
-	const data = {};
+	const dir = __dirname;
+	await fs.ensureFile($path.resolve(dir, "./settings.json"));
+	const settings = JSON.parse((await fs.readFile($path.resolve(dir, "./settings.json"), "utf8")) || "{}");
 
 	try {
-		data.name = await readln("Enter project name: ");
-		data.owner = await readln("Enter github project owner: ");
-		data.src = path.resolve(__dirname, "./project");
-		data.dest = path.resolve(cwd, `./${data.name}`);
-		await fs.copy(data.src, data.dest, {overwrite: true});
-		const pkg = JSON.parse((await fs.readFile(path.resolve(data.dest, "./package.json"), "utf8")).replace(/projectname/gm, data.name).replace(/projectowner/gm, data.owner));
-		pkg["va-release"].owner = data.owner;
+		console.log("Let's create a new project", ":)".green);
+		const questions = [
+			{
+				type: "input",
+				name: "name",
+				message: "project name",
+				default: settings.name,
+				validate: val => (val || "").trim().length > 0,
+			},
+			{
+				type: "input",
+				name: "owner",
+				default: settings.owner,
+				message: "github project owner",
+			},
+			{
+				type: "list",
+				choices: ["yes", "no"],
+				name: "isClient",
+				default: settings.isClient,
+				message: "client web app code",
+			},
+			{
+				type: "list",
+				choices: ["yes", "no"],
+				name: "isServer",
+				default: settings.isServer,
+				message: "server code",
+			},
+			{
+				type: "list",
+				choices: ["yes", "no"],
+				name: "isCLI",
+				default: settings.isCLI,
+				message: "command line interface (cli) code",
+			},
+			{
+				type: "list",
+				choices: ["yes", "no"],
+				name: "isLib",
+				default: settings.isLib,
+				message: "basic js library code",
+			},
+		];
+		const data = await inquirer.prompt(questions);
+		Object.assign(settings, data);
+
+		const flags = [];
+		if (data.isClient === "yes") {
+			flags.push("client");
+		}
+		if (data.isServer === "yes") {
+			flags.push("server");
+		}
+		if (data.isCLI === "yes") {
+			flags.push("cli");
+		}
+		if (data.isLib === "yes") {
+			flags.push("lib");
+		}
+
+
+		await fs.writeFile($path.resolve(dir, "./settings.json"), JSON.stringify(settings, null, "\t"), "utf8");
+
+		data.src = $path.resolve(__dirname, "./project");
+		data.dest = $path.resolve(cwd, `./${data.name}`);
+		await copy(data.src, data.dest, flags);
+
+		const pkg = JSON.parse((await fs.readFile($path.resolve(data.dest, "./package.json"), "utf8")).replace(/projectname/gm, data.name).replace(/projectowner/gm, data.owner));
 		pkg["va-release"].library = camelcase(data.name);
-		await fs.writeFile(path.resolve(data.dest, "./package.json"), JSON.stringify(pkg, null, "\t"), "utf8");
+
+
+		await fs.writeFile($path.resolve(data.dest, "./package.json"), JSON.stringify(pkg, null, "\t"), "utf8");
+
 		console.log(`${"Success!".green} Project ${`${data.name}`.cyan} created at ${`${data.dest}`.cyan}`);
 		process.exit(0);
 	}
 	catch (error) {
-		console.log(`${"Error!".red} Failed to create project ${`${data.name}`.cyan} at ${`${data.dest}`.cyan}`);
+		console.log(`${"Error!".red} Failed to create project`);
+		console.log(error);
 		process.exit(1);
 	}
-	return data;
 }
 
 main();
---
+++ tttt/templates/README.md
@@ -0,0 +1,70 @@
+# {{package.name}}
+
+[![npm repository](https://img.shields.io/npm/v/{{package.name}}.svg)](https://www.npmjs.com/package/{{package.name}})
+[![Build Status](https://travis-ci.org/{{repository.owner}}/{{repository.name}}.svg?branch=master)](https://travis-ci.org/{{repository.owner}}/{{repository.name}})
+[![Coverage Status](https://img.shields.io/codecov/c/github/{{repository.owner}}/{{repository.name}}/master.svg)](https://codecov.io/gh/{{repository.owner}}/{{repository.name}})
+[![Known Vulnerabilities](https://snyk.io/test/github/{{repository.owner}}/{{repository.name}}/badge.svg)](https://snyk.io/test/github/{{repository.owner}}/{{repository.name}})
+
+{{package.description}}
+
+## Installation
+
+#### Install as npm package
+
+```shell
+npm i {{package.name}} --save
+```
+
+#### Or simply download \*.js file
+
+[{{package.name}}.js@{{version}}](https://github.com/{{repository.owner}}/{{repository.name}}/releases/download/{{version}}/{{package.name}}.js)
+
+[{{package.name}}.modern.js@{{version}}](https://github.com/{{repository.owner}}/{{repository.name}}/releases/download/{{version}}/{{package.name}}.modern.js) *for modern browsers only (see [.browserlistrc](https://github.com/{{repository.owner}}/{{repository.name}}/blob/master/.browserslistrc))*
+
+#### Or just load from CDN
+
+```html
+<script src="//cdn.jsdelivr.net/npm/{{package.name}}@{{version}}/dist/{{package.name}}.js" integrity="{{#ssri}}../dist/{{package.name}}.js{{/ssri}}" crossorigin="anonymous">
+</script>
+```
+
+*if you need only modern browsers then use script below:*
+
+```html
+<script src="//cdn.jsdelivr.net/npm/{{package.name}}@{{version}}/dist/{{package.name}}.modern.js" integrity="{{#ssri}}../dist/{{package.name}}.modern.js{{/ssri}}" crossorigin="anonymous">
+</script>
+```
+
+And then use **{{package["va-release"].library}}** as global variable
+```html
+<script>
+	console.log({{package["va-release"].library}});
+</script>
+```
+## [Documentation](./DOCUMENTATION.md)
+
+## Example
+
+Run example with [runkit](https://npm.runkit.com/{{package.name}})
+
+```js
+{{#file}}../tonic-example.js{{/file}}
+```
+
+## Compatibility
+
+#### Browsers
+
+| Chrome | Edge | Firefox | Internet Explorer | Opera | Safari |
+| ------ | ---- | ------- | ----------------- | ----- | ------ |
+| 49     | 12   | 18      | *No support*      | 36    | 10     |
+
+#### Servers/runtimes
+
+Supported on **Node.js** version **6** and higher
+
+#### Proxy compatibility tables
+
+https://kangax.github.io/compat-table/es6/#test-Proxy
+
+http://caniuse.com/#feat=proxy
